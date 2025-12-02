15b8638e473f343ceb41a7d6d2d9b166fbc8e4db
Jing Guo <dev.guoj@gmail.com>
2025-12-02 15:25:55 +0800

    complete and include both versions

--- day-two.py
+++ day-two.py
@@ -1,3 +1,43 @@
+# My original version
+
+print('My original version')
+
+with open('day-two.txt') as f:
+    num_seq = f.read().split(',')
+
+num_seq = [tuple(map(int, num.split('-'))) for num in num_seq]
+part_one_invalid_seq = []
+part_two_invalid_seq = []
+
+def part_one_is_invalid(n):
+    s = str(n)
+    mid = len(s) // 2
+    return len(s) % 2 == 0 and s[:mid] == s[mid:]
+
+def part_two_is_invalid(n):
+    s = str(n)
+
+    for divided in range(2, len(s) + 1):
+        if len(s) % divided == 0:
+            part = s[: len(s) // divided]
+            if part * divided == s:
+                return True
+    return False
+
+for start_num, end_num in num_seq:
+    for num in range(start_num, end_num + 1):
+        if part_one_is_invalid(num):
+            part_one_invalid_seq.append(num)
+        if part_two_is_invalid(num):
+            part_two_invalid_seq.append(num)
+
+print(sum(part_one_invalid_seq))
+print(sum(part_two_invalid_seq))
+
+# Claude improved version
+
+print('Claude improved version')
+
 def parse_ranges(filename: str) -> list[tuple[int, int]]:
     with open(filename) as f:
         ranges = f.read().split(',')
