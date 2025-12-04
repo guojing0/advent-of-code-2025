dbdef19bc97523380a888cf0d2f1b59034edb575
Jing Guo <dev.guoj@gmail.com>
2025-12-04 15:55:48 +0800

    complete both parts

--- day-four.py
+++ day-four.py
@@ -12,7 +12,26 @@ kernel = np.array([[1, 1, 1],
                    [1, 0, 1],
                    [1, 1, 1]])
 
+### First part
+
 nbhr_counts = convolve(grid, kernel, mode='constant', cval=0)
 
 # Count @ cells with fewer than 4 @ neighbors
 print(np.sum((grid == 1) & (nbhr_counts < 4)))
+
+### Second part
+
+total_count = 0
+
+while True:
+    nbhr_counts = convolve(grid, kernel, mode='constant', cval=0)
+    current_count = np.sum((grid == 1) & (nbhr_counts < 4))
+
+    if current_count == 0:
+        break
+
+    total_count += current_count
+
+    grid = np.where((grid == 1) & (nbhr_counts < 4), 0, grid)
+
+print(total_count)
